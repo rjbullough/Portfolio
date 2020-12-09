bd2266efb20ade16285d4a87935bffec9633b46d
Ryan Bullough <rjbullough@gmail.com>
2020-12-10 10:23:41 +1100

    Update grammar in about me

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,12 +18,11 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: `I've recently made the transition from coal to code.`,
+  paragraphOne: `After working in the mining industry for 15 years I've made the transition to code.`,
   paragraphTwo: `At some point in your life you realise there's merit in
    chasing after what you're passionate about.`,
-  paragraphThree: `I love solving problems and working with creative people. Fortunately for me - software engineering provides both in abundance!`,
-  paragraphFour: `Browse below to see a few samples of my work, check out my resume or flick me an email - 
-   I'd love to chat.`,
+  paragraphThree: `I love solving problems and working with creative people. Fortunately for me, software engineering provides both in abundance!`,
+  paragraphFour: `Take a look at some of my projects below, check out my resume or flick me an email. I'd love to chat.`,
   resume: 'https://github.com/rjbullough/Resume/blob/main/Ryan%20Bullough%20-%20Resume.pdf', // if no resume, the button will not show up
 };
 
@@ -43,7 +42,7 @@ export const projectsData = [
     img: 'UNA.jpeg',
     title: 'UNA',
     info:
-      'Nutritional tracking web app that features beautiful data representation and simple inputs ',
+      'Daily food intake tracking app that features beautiful data representation and intuitive design',
     info2: 'Built using Ruby on Rails',
     url: 'https://bit.ly/2HInsBs',
     repo: 'https://github.com/rjbullough/seic38-project1', // if no repo, the button will not show up
